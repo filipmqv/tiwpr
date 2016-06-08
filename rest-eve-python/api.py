fe0ee4a6e05f9a34ee6461b67b89a4a9dcbc142a
Filip Waligórski <filip.waligorski@student.put.poznan.pl>
2016-06-08 21:32:11 +0200

    renaming, tidy up

---
+++ rest-eve-python/api.py
@@ -0,0 +1,110 @@
+from eve import Eve
+from eve.auth import BasicAuth
+from flask import Flask, jsonify, request
+import base64
+
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
+
+# snippet for flask crossdomain
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+# end of snippet for flask crossdomain
+
+
+
+
+class Authenticate(BasicAuth):
+	def check_auth(self, email, password, allowed_roles, resource, method):
+		#print "entering"
+		users = app.data.driver.db['users']
+		user = users.find_one({'email': email,'password': password})
+		if user:
+			if user.get('role') == 'student':
+				#some_list = ['abc-123', 'def-456', 'ghi-789', 'abc-456']
+				#print "student"
+				return True
+			elif user.get('role') == 'teacher':
+				return True
+			elif user.get('role') == 'parent':
+				return True
+			else:
+				#print "not allowed"
+				return False
+		elif email == 'admin' and password == 'password':
+			return True
+		else:
+			#print "not user"
+			return False
+
+
+app = Eve(auth=Authenticate)
+
+
+@app.route('/login', methods = ['POST', 'OPTIONS'])
+@crossdomain(origin='*',headers=['Content-Type','Authorization'])
+def login():
+	data = request.get_json()
+	email = data.get('email')
+	password = data.get('password')
+	users = app.data.driver.db['users']
+	user = users.find_one({'email': email,'password': password})
+	if user:
+		hashed = base64.b64encode(email+":"+password)
+		resp = jsonify(id = str(user.get('_id')), firstname = user.get('firstname'), auth = 'Basic '+hashed, 
+			role = user.get('role'))
+		resp.status_code = 200
+		return resp
+	else:
+		resp = jsonify(error = 'Wrong email or password.')
+		resp.status_code = 401
+		return resp
+
+def delete_grades(item):
+	grades = app.data.driver.db['grades']
+	ok = grades.remove({'test_id': item.get('_id')})
+	print "removing grades of this test"
+	print ok
+
+if __name__ == '__main__':
+	app.on_delete_item_tests += delete_grades
+	app.run(threaded=True)
---
+++ rest-eve-python/settings.py
@@ -0,0 +1,349 @@
+RESOURCE_METHODS = ['GET','POST'] # dla calej kolekcji
+
+ITEM_METHODS = ['GET','PUT','DELETE'] # dla konkretnego id
+
+X_DOMAINS = '*'
+X_HEADERS = ['Authorization','If-Match','Access-Control-Expose-Headers','Content-Type','Pragma','Cache-Control']
+X_EXPOSE_HEADERS = ['Origin', 'X-Requested-With', 'Content-Type', 'Accept']
+CACHE_CONTROL = 'max-age=1,must-revalidate'
+
+PAGINATION = False
+PAGINATION_LIMIT = 9999999
+PAGINATION_DEFAULT = 25
+
+DATE_FORMAT ='%Y-%m-%dT%H:%M:%S'
+
+users = {
+    'item_title': 'user',
+    'additional_lookup': {
+        'url': 'regex("[\w]+")',
+        'field': 'email',
+    },
+    'datasource': {
+        'projection': {'password': 0},
+        'filter': {'firstname': {'$ne': 'dummy'}}
+    },
+    'schema': {
+        'firstname': {
+            'type': 'string',
+            'required': True
+        },
+        'lastname': {
+            'type': 'string',
+            'required': True
+        },
+        'email': {
+            'type': 'string',
+            'unique': True
+        },
+        'password': {
+            'type': 'string',
+            'required': True
+        },
+        'role': {
+            'type': 'string',
+            'allowed': ["student", "teacher", "parent"],
+            'required': True
+        },
+        # fields for student
+        'class_id': {
+            'type': 'objectid',
+            'data_relation': {
+                 'resource': 'classes',
+                 'field': '_id',
+                 'embeddable': True
+            },
+        },
+        'registrynumber': {
+            'type': 'integer'
+        },
+        # fields for teacher
+
+        # fields for parent
+        'children_id': {
+            'type': 'list', 
+            'schema': {
+                'type': 'objectid',
+                    'data_relation': { 
+                        'resource': 'users',
+                        'field': '_id', 
+                        'embeddable': True
+                } 
+            }
+        }
+        
+    }
+}
+
+students = {
+    'pagination': True,
+    'datasource': {
+        'source': 'users',
+        'filter': {'role': 'student'},
+        'projection': {'password': 0},
+        'default_sort': [('class_id', 1), ('lastname',1)]
+    }
+}
+
+students_all = {
+    'datasource': {
+        'source': 'users',
+        'filter': {'role': 'student'},
+        'projection': {'password': 0},
+        'default_sort': [('class_id', 1)]
+    }
+}
+
+grades_schema = {
+    'gradevalue': {
+        'type': 'string',
+        'allowed': ["1", "1+", "2-", "2", "2+", "3-", "3", "3+", "4-", "4", "4+", "5-", "5", "5+", "6-", "6", "6+"],
+        'required': True
+    },
+    'student_id': {
+        'type': 'objectid',
+        'data_relation': {
+             'resource': 'users',
+             'field': '_id',
+             'embeddable': True
+        },
+        'required': True
+    },
+    'test_id': {
+        'type': 'objectid',
+        'data_relation': {
+             'resource': 'tests',
+             'field': '_id',
+             'embeddable': True
+        },
+        'required': True
+    }
+}
+
+grades = {
+    'item_title': 'grade',
+    'schema': grades_schema
+}
+
+students_grades = {
+    'url': 'students/<regex("[a-f0-9]{24}"):student_id>/grades',
+    'schema': grades_schema,
+    "datasource": {"source": "grades"},
+    'hateoas': False
+}
+
+
+classes = {
+    'item_title': 'class',
+    'schema': {
+        'name': {
+            'type': 'string',
+            'unique': True
+        }
+    },
+    'datasource': {
+        'default_sort': [('name', 1)],
+        'filter': {'name': {'$ne': 'dummy'}}
+    },
+    'hateoas': False
+}
+
+subjects = {
+    'item_title': 'subject',
+    'schema': {
+        'name': {
+            'type': 'string',
+            'unique': True
+        }
+    },
+    'datasource': {
+        'default_sort': [('name', 1)],
+        'filter': {'name': {'$ne': 'dummy'}}
+    }
+}
+
+# test to coś co można zapowiedzieć
+tests = {
+    'item_title': 'test',
+    'schema': {
+        'testtype': {
+            'type': 'string',
+            'allowed': ["short test", "homework", "essay"],
+            'required': True
+        },
+        'name': {
+            'type': 'string',
+            'required': True
+        },
+        'testdate': {
+            'type': 'datetime',
+            'required': True
+        },
+        'class_id': {
+            'type': 'objectid',
+            'data_relation': {
+                 'resource': 'classes',
+                 'field': '_id',
+                 'embeddable': True
+            },
+        },
+        'subject_id': {
+            'type': 'objectid',
+            'data_relation': {
+                 'resource': 'subjects',
+                 'field': '_id',
+                 'embeddable': True
+            },
+        },
+        'teacher_id': {
+            'type': 'objectid',
+            'data_relation': {
+                 'resource': 'users',
+                 'field': '_id',
+                 'embeddable': True
+            },
+        },
+        'status': { # 0-zapowiedziany, 1-oceniony (oceny automatycznie usuwane)
+            'type': 'integer', 
+            'required': True
+        }
+    },
+    'datasource': {
+        'default_sort': [('testdate', -1)]
+    }
+}
+#typ(spr, kart, odp, zad. domowe), tytuł, data, przedmiot, klasa(czyli uczniowie), nauczyciel
+
+attendances_schema = {
+    'student_id': {
+        'type': 'objectid',
+        'data_relation': {
+             'resource': 'users',
+             'field': '_id',
+             'embeddable': True
+        },
+        'required': True
+    },
+    'lesson_id': {
+        'type': 'objectid',
+        'data_relation': {
+             'resource': 'lessons',
+             'field': '_id',
+             'embeddable': True
+        },
+        'required': True
+    },
+    'status': {
+        'type': 'string',
+        'allowed': ["absent", "present", "justified"], #nieobecny (czyli nieuspr.), obecny, usprawiedliwony
+        'required': True
+    },
+    'justification': {
+        'type': 'string'
+    }
+}
+
+attendances = {
+    'item_title': 'attendance',
+    'schema': attendances_schema,
+    'datasource': {
+        'default_sort': [('lesson_id', -1)]
+    }
+}
+
+students_attendances = {
+    'url': 'students/<regex("[a-f0-9]{24}"):student_id>/attendances',
+    'schema': attendances_schema,
+    "datasource": {
+        "source": "attendances",
+        'default_sort': [('lesson_id', -1)]
+    },
+    'hateoas': False
+}
+
+absences = {
+    'url': 'students/<regex("[a-f0-9]{24}"):student_id>/absences',
+    'schema': attendances_schema,
+    "datasource": {
+        "source": "attendances",
+        'filter': {'status': 'absent'}
+    },
+    'hateoas': False
+}
+
+lessons_schema = {
+    'lessondate': {
+            'type': 'datetime',
+            'required': True
+    },
+    'class_id': {
+        'type': 'objectid',
+        'data_relation': {
+             'resource': 'classes',
+             'field': '_id',
+             'embeddable': True
+        }
+    },
+    'teacher_id': {
+        'type': 'objectid',
+        'data_relation': {
+             'resource': 'users',
+             'field': '_id',
+             'embeddable': True
+        }
+    },
+    'subject_id': {
+        'type': 'objectid',
+        'data_relation': {
+             'resource': 'subjects',
+             'field': '_id',
+             'embeddable': True
+        },
+    }
+}
+
+lessons = {
+    'item_title': 'lesson',
+    'pagination': True,
+    'schema': lessons_schema,
+    'datasource': {
+        'default_sort': [('lessondate', -1)]
+    }
+}
+
+lessonsid = {
+    'schema': {
+        'lessondate': {
+                'type': 'datetime',
+                'required': False
+        },
+        'class_id': {
+            'type': 'objectid'
+        },
+        'teacher_id': {
+            'type': 'objectid'
+        },
+        'subject_id': {
+            'type': 'objectid'
+        }
+    },
+    'datasource': {"source": "lessons"},
+    'resource_methods': ['POST'],
+    'item_methods': []
+}
+
+DOMAIN = {
+    'users': users, 
+    'students': students,
+    'students_all': students_all,
+    'grades': grades,
+    'students_grades': students_grades,
+    'classes': classes, 
+    'subjects': subjects,
+    'tests': tests,
+    'attendances': attendances,
+    'students_attendances': students_attendances,
+    'absences': absences,
+    'lessons': lessons,
+    'lessonsid': lessonsid
+}
