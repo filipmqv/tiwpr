728d93e47da46fae456b070b0df641ad58bb6f26
Filip Waligórski <filip.waligorski@student.put.poznan.pl>
2016-06-15 00:52:27 +0200

    game up to 100 points, showing winner, staring new game

--- websocket-nodejs-socketio/public/javascripts/script.js
+++ websocket-nodejs-socketio/public/javascripts/script.js
@@ -32,6 +32,16 @@ var text = new fabric.Text('', {
 });
 canvas.add(text);
 
+var gameFinishedText = new fabric.Text('', { 
+	left: 50, 
+	top: 50, 
+	fontSize: 60,
+	selectable: false,
+	evented: false,
+	textAlign: 'center',
+	hoverCursor: 'default'
+});
+
 
 
 
@@ -87,30 +97,11 @@ socket.on('remove object', function (data) {
 });
 
 socket.on('new points', function (data) {
-	var pointsRect = new fabric.Text('+'+data.amount, {  
-		left: getRandomInt((data.X - 150) < 0 ? 0 : (data.X - 150), 
+	animateText('+'+data.amount, 
+		getRandomInt((data.X - 150) < 0 ? 0 : (data.X - 150), 
 			(data.X + 150) < (window.innerWidth - 150) ? (data.X + 50) : (window.innerWidth - 150)), 
-		top: getRandomInt((data.Y - 150) < 0 ? 0 : (data.Y - 150), 
-			(data.Y + 150) < (window.innerHeight - 150) ? (data.Y + 50) : (window.innerHeight - 150)), 
-		//left: getRandomInt(50, window.innerWidth - 150),
-		//top: getRandomInt(50, window.innerHeight - 150),
-		fontSize: 50,
-		selectable: false,
-		evented: false,
-		hoverCursor: 'default'
-	});
-	pointsRect.animate('fontSize', '+=100', {
-		onChange: canvas.renderAll.bind(canvas),
-		duration: 500,
-		easing: fabric.util.ease.easeInOutQuad,
-		onComplete: function() {canvas.remove(pointsRect)}
-	});
-	pointsRect.animate('opacity', 0, {
-		onChange: canvas.renderAll.bind(canvas),
-		duration: 500,
-		easing: fabric.util.ease.easeInOutQuad
-	});
-	canvas.add(pointsRect);
+		getRandomInt((data.Y - 150) < 0 ? 0 : (data.Y - 150), 
+			(data.Y + 150) < (window.innerHeight - 150) ? (data.Y + 50) : (window.innerHeight - 150)), 500);
 });
 
 socket.on('user clicked', function (data) {
@@ -130,13 +121,41 @@ socket.on('user clicked', function (data) {
 	canvas.add(clickCircle);
 });
 
+socket.on('game finished', function (data) {
+	for (var o in canvasObjects) {
+		canvasObjects[o].set({selectable: false, evented: false});
+	}
+	gameFinishedText.setText('GAME FINISHED\n\nWINNER: ' + data.winner);
+	canvas.remove(gameFinishedText);
+	canvas.add(gameFinishedText);
+	gameFinishedText.set({'left': Math.ceil((window.innerWidth / 2) - (gameFinishedText.getWidth() / 2)),
+		'top': Math.ceil((window.innerHeight / 2) - (gameFinishedText.getHeight() / 2))});
+	canvas.renderAll();
+});
+
+socket.on('game ready', function (data) {
+	animateText(data.timeLeft, Math.ceil(0.94 * window.innerWidth / 2), 
+		Math.ceil(0.8 * window.innerHeight / 2), 500);
+});
+
+socket.on('game start', function () {
+	animateText('START!', Math.ceil(0.73 * window.innerWidth / 2), 
+		Math.ceil(0.8 * window.innerHeight / 2), 500);
+});
+
+socket.on('new game', function () {
+	// TODO clear all obj
+	for (var o in canvasObjects) {
+		canvas.remove(canvasObjects[o]);
+        delete canvasObjects[o];
+	}
+	canvas.remove(gameFinishedText);
+});
+
 /*socket.on('begin', function (data) {
 	rect.set(data);
 	canvas.renderAll();
 });
-
-
-
 
 
 socket.on('refreshUpdate', function (data) {
@@ -148,13 +167,27 @@ socket.on('disconnect', function (data) {
 	location.reload();
 });
 
-/*setInterval(function() {
-	socket.emit('refresh', {
-		left: rect.getLeft(), 
-		top: rect.getTop(), 
-		width: rect.getWidth(), 
-		height: rect.getHeight(),
-  		angle: rect.getAngle(),
-  		fill: rect.getFill()
+
+
+function animateText(textToShow, left, top, time) {
+	var animText = new fabric.Text(''+textToShow, {  
+		left: left, 
+		top: top, 
+		fontSize: 50,
+		selectable: false,
+		evented: false,
+		hoverCursor: 'default'
 	});
-}, 80);*/
+	animText.animate('fontSize', '+=100', {
+		onChange: canvas.renderAll.bind(canvas),
+		duration: time,
+		easing: fabric.util.ease.easeInOutQuad,
+		onComplete: function() {canvas.remove(animText)}
+	});
+	animText.animate('opacity', 0, {
+		onChange: canvas.renderAll.bind(canvas),
+		duration: time,
+		easing: fabric.util.ease.easeInOutQuad
+	});
+	canvas.add(animText);
+}
--- websocket-nodejs-socketio/sockets.js
+++ websocket-nodejs-socketio/sockets.js
@@ -2,6 +2,7 @@ module.exports = function(server){
     var socketio = require('socket.io')(server);
     var sockets = {};
     var rooms = {};
+    var POINTS_LIMIT = 100;
 
     socketio.sockets.on('connection', function(socket) {
         // if there's no cookie and no userId in cookie - disconnect
@@ -37,20 +38,29 @@ module.exports = function(server){
                 rooms[userRoomName] = {};
                 rooms[userRoomName].users = {};
                 rooms[userRoomName].objects = {};
+                switchGameToReady(userRoomName);
             }
             sockets[userId].roomName = data.roomName;
             sockets[userId].userName = data.userName;
             sockets[userId].cHeight = data.cHeight;
             sockets[userId].cWidth = data.cWidth;
             if (!sockets[userId].points) {
+                // if for the first time on click-io
                 sockets[userId].points = {};
             }
-            if (!sockets[userId].points[userRoomName]) {
-                sockets[userId].points[userRoomName] = 0;
+            if (!sockets[userId].points[userRoomName] 
+                    || sockets[userId].points[userRoomName].gameId != rooms[userRoomName].gameId) {
+                // if not previously in this room or game id has changed (new game)
+                sockets[userId].points[userRoomName] = {};
+                sockets[userId].points[userRoomName].value = 0;
+                sockets[userId].points[userRoomName].gameId = rooms[userRoomName].gameId;
             }
-            rooms[userRoomName].users[userId] = 'dummy';
+            rooms[userRoomName].users[userId] = 'dummy'; // just to keep userIds
             emitPlayers(userRoomName);
             emitAllObjects(userRoomName);
+            if (rooms[userRoomName].gameState == 'finished') {
+                socket.emit('game finished', {winner: rooms[userRoomName].winner});
+            }
         });
 
         socket.on('game object click', function (data) {
@@ -59,15 +69,19 @@ module.exports = function(server){
                 emitInRoom(userRoomName, 'remove object', {id: data.id});
                 var tempObj = rooms[userRoomName].objects[data.id].props;
                 var newPoints = Math.ceil(1000 / (tempObj.width * tempObj.height))
-                sockets[userId].points[userRoomName] += newPoints;
+                sockets[userId].points[userRoomName].value += newPoints;
                 socket.emit('new points', {amount: newPoints, X: data.X, Y: data.Y});
                 emitPlayers(userRoomName);
                 delete rooms[userRoomName].objects[data.id];
+                if (sockets[userId].points[userRoomName].value >= POINTS_LIMIT) {
+                    switchGameToFinished(userRoomName, userName);
+                }
             }
         });
 
         socket.on('any click', function (data) {
-            emitInRoom(userRoomName, 'user clicked', {X: data.X, Y: data.Y});
+            // volatile, not always delivered, but better performance
+            emitVolatileInRoom(userRoomName, 'user clicked', {X: data.X, Y: data.Y});
         });
 
         socket.on('resize', function (data) {
@@ -90,7 +104,7 @@ module.exports = function(server){
         function getPlayersFromRoom(rName) {
             var players = [];
             for(var uId in rooms[rName].users) {
-                players.push({userName: sockets[uId].userName, points: sockets[uId].points[userRoomName]});
+                players.push({userName: sockets[uId].userName, points: sockets[uId].points[userRoomName].value});
             }
             players.sort(function(a,b) {return (a.points > b.points) ? -1 : ((b.points > a.points) ? 1 : 0);} );
             return players;
@@ -112,19 +126,61 @@ module.exports = function(server){
             }
         }
 
+        function emitVolatileInRoom(rName, tag, emitObject) {
+            for(var uId in rooms[rName].users) {
+                sockets[uId].socket.volatile.emit(tag, emitObject);
+            }
+        }
 
+        function getRandomGameId() {
+            var randomNumber=Math.random().toString()+'00000000000000000000';
+            return randomNumber.substring(2,20);
+        }
 
-        
+        function switchGameToReady(rName) {
+            rooms[rName].gameId = getRandomGameId();
+            rooms[rName].gameState = 'ready'; //ready(10s) -> playing -> finished(5s)
+            for (var o in rooms[rName].objects)
+                delete rooms[rName].objects[o];
+            for (var uId in rooms[rName].users) {
+                sockets[uId].points[rName].value = 0;
+                sockets[uId].points[rName].gameId = rooms[rName].gameId;
+            }
+            emitInRoom(rName, 'new game', {});
+            emitPlayers(rName);
+
+            for (var i = 0; i < 10; i++) {
+                setTimeout(function(rName, i) {
+                    emitInRoom(rName, 'game ready', {timeLeft: 10-i});
+                }, i*1000, rName, i);
+            }
+            setTimeout(function(rName) {
+                switchGameToPlaying(rName);
+            }, 10000, rName);
+        }
+
+        function switchGameToPlaying(rName) {
+            rooms[rName].gameState = 'playing';
+            emitInRoom(rName, 'game start', {});
+        }
+
+        function switchGameToFinished(rName, uName) {
+            rooms[rName].gameState = 'finished';
+            rooms[rName].winner = uName;
+            emitInRoom(rName, 'game finished', {winner: uName});
+            for (var uId in rooms[rName].users) {
+                sockets[uId].points[rName].value = 0;
+            }
+            for (var o in rooms[rName].objects)
+                rooms[rName].objects[o].props.evented = false;
+            setTimeout(function(rName) {
+                switchGameToReady(rName);
+            }, 5000, rName);
+        }
     });
 
-    /*
-    id: sadasdasd
-    left: 30,
-    top: 30,
-    fill: 'green',
-    width: 20,
-    height: 20,
-    selectable: false*/
+    
+
 
     function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -161,14 +217,14 @@ module.exports = function(server){
         return result;
     }
 
-    var objectId = 0;
+    var objectIdGenerator = 0;
     function generateObjects() {
         for (var rName in rooms) {
-            if (Object.keys(rooms[rName].objects).length < 75) {
+            if (rooms[rName].gameState == 'playing' && Object.keys(rooms[rName].objects).length < 75) {
                 var maxDimens = getMaxDimensions(rName);
                 var generated = {};
                 generated.type = 'rect';
-                generated.props = generateObject(objectId++, maxDimens.width, maxDimens.height);
+                generated.props = generateObject(objectIdGenerator++, maxDimens.width, maxDimens.height);
                 rooms[rName].objects[generated.props.id] = generated;
                 for(var uId in rooms[rName].users) {
                     sockets[uId].socket.emit('new object', generated);
